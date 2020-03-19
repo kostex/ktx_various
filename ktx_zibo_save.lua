a21c1da88a53dfcad43f627ccf8cc75546fd5dbc
Roel Koster <kostex@users.noreply.github.com>
2020-03-19 09:53:20 +0100

    Updated Interface and Hide Button

--- ktx_zibo_save.lua
+++ ktx_zibo_save.lua
@@ -11,6 +11,8 @@ local filelist = {}
 local save_file_exists = {}
 local filecount = 0
 local line = ""
+local offset = 70
+local istate = false
 local enter_name = false
 local file_item_clicked = 0
 local save_file_route = {}
@@ -18,19 +20,19 @@ local filename_regel = "Enter_Save_FileName_Here"
 
 
 function ktx_draw_rect(x,y,w,h)
-  graphics.draw_rectangle(x, SCREEN_HIGHT - y, x + w, SCREEN_HIGHT - y - h)
+  graphics.draw_rectangle(x, SCREEN_HIGHT - y, x + w, SCREEN_HIGHT - y + h)
 end
 
 function ktx_draw_string(x,y,s,color)
   graphics.draw_string(x, SCREEN_HIGHT - y, s,color)
 end
 
-function ktx_draw_button(x,y,s,size,r1,g1,b1,color)
-  graphics.set_color(r1,g1,b1)
+function ktx_draw_button(x,y,s,size,r1,g1,b1,a1,color)
+  graphics.set_color(r1,g1,b1,a1)
   if size == 0 then
-    ktx_draw_rect(x,y,measure_string(s)+4,-16)
+    ktx_draw_rect(x,y,measure_string(s)+4,16)
   else
-    ktx_draw_rect(x,y,size,-16)
+    ktx_draw_rect(x,y,size,16)
   end
   ktx_draw_string(x+2,y-4,s,color)
 end
@@ -38,13 +40,13 @@ end
 function ktx_draw_field(x,y,s,size,r1,g1,b1,color)
   graphics.set_color(r1,g1,b1)
   if size == 0 then
-    ktx_draw_rect(x,y,measure_string(s)+4,-16)
+    ktx_draw_rect(x,y,measure_string(s)+4,16)
   else
-    ktx_draw_rect(x,y,size,-16)
+    ktx_draw_rect(x,y,size,16)
   end
   if enter_name then
     ktx_c("red")
-    ktx_draw_rect(x+measure_string(s),y,2,-16)
+    ktx_draw_rect(x+measure_string(s),y,2,16)
   end
   ktx_draw_string(x+2,y-4,s,color)
 end
@@ -71,52 +73,58 @@ function ktx_mouse_y()
 end
 
 function draw_ktx_save()
-  ktx_draw_button(10,20,"ZIBO LOAD/SAVE FLIGHT EXTENSION",800,0.2,0.2,0.2,"white")
+  if istate then
+    -- interface shows
+    ktx_draw_button(10,20+offset,"(X) KTX ZIBO LOAD/SAVE FLIGHT EXTENSION",800,0.2,0.2,0.2,1,"white")
 
-  -- Black Background
-  ktx_c("black")
-  if filecount > 7 then
-    ktx_draw_rect(10, 20 ,800, 55 + filecount*20)
-  else
-    ktx_draw_rect(10, 20 ,800, 200)
-  end
-
-  ktx_draw_button(20,50,"Refresh Situations",350,0.6,0.9,0.6,"black")
-
-  y = 80
-  x = 20
-  for i=0,filecount-1 do
-    if file_item_clicked == i+1 then
-      ktx_draw_button(x, y, filelist[i+1][1].." ("..filelist[i+1][2]..")",350,0.5,0.5,0.5,"white")
-      ktx_c("red")
-      ktx_draw_rect(x+350,y,5,-16)
+    -- Black Background
+    graphics.set_color(0,0,0,0.3)
+    if filecount > 7 then
+      ktx_draw_rect(10, 20+offset ,800, -(55 + filecount*20))
     else
-      ktx_draw_button(x, y, filelist[i+1][1],350,0.4,0.4,0.4,"white")
+      ktx_draw_rect(10, 20+offset ,800, -200)
     end
-    y = y + 20
-  end
 
-  y = 80
-  x = 400
-  if file_item_clicked > 0 then
-    for i=1,4 do
-      status=string.format("Load in %d", i)
-      ktx_draw_button(x, y, status, 100,0,0.5,0,"white")
-      y = y + 20
-    end
-    ktx_draw_button(x, y + 20, "Delete Situation", 100,1,0,0,"white")
-  else
-    ktx_draw_field(x,50,filename_regel,390,1,1,1,"black")
-    for i=1,4 do
-      if save_file_exists[i] then
-        if filename_regel ~= "Enter_Save_FileName_Here" and filename_regel ~= "" then
-          ktx_draw_button(x + 110, y, "SaveAs",50,0,0.5,0,"white")
-        end
+    ktx_draw_button(20,50+offset,"Refresh Situations",350,0.6,0.9,0.6,1,"black")
+
+    y = 80+offset
+    x = 20
+    for i=0,filecount-1 do
+      if file_item_clicked == i+1 then
+        ktx_draw_button(x, y, filelist[i+1][1].." ("..filelist[i+1][2]..")",350,0.5,0.5,0.5,1,"white")
+        ktx_c("red")
+        ktx_draw_rect(x+350,y,5,16)
+      else
+        ktx_draw_button(x, y, filelist[i+1][1],350,0.4,0.4,0.4,1,"white")
       end
-      status=save_file_route[i]
-      ktx_draw_button(x, y, status, 100,0.3,0.3,0.3,"white")
       y = y + 20
     end
+
+    y = 80+offset
+    x = 400
+    if file_item_clicked > 0 then
+      for i=1,4 do
+        status=string.format("Load in %d", i)
+        ktx_draw_button(x, y, status, 100,0,0.5,0,1,"white")
+        y = y + 20
+      end
+      ktx_draw_button(x, y + 20, "Delete Situation", 100,1,0,0,1,"white")
+    else
+      ktx_draw_field(x,50+offset,filename_regel,390,1,1,1,"black")
+      for i=1,4 do
+        if save_file_exists[i] then
+          if filename_regel ~= "Enter_Save_FileName_Here" and filename_regel ~= "" then
+            ktx_draw_button(x + 110, y, "SaveAs",50,0,0.5,0,1,"white")
+          end
+        end
+        status=save_file_route[i]
+        ktx_draw_button(x, y, status, 100,0.3,0.3,0.3,1,"white")
+        y = y + 20
+      end
+    end
+  else
+    -- Show HotSpot when mouse over
+    ktx_draw_button(0,offset+20,"KTX_Zibo_Save",90,0.5,0.5,0.5,0.5,"white")
   end
 end
 
@@ -177,56 +185,71 @@ function refresh()
 end
 
 function mouse_check()
-  if MOUSE_STATUS ~= "down" then
-    return
-  end
-
-  if MOUSE_X > 20 and MOUSE_X < 370 and ktx_mouse_y() > 30 and ktx_mouse_y() < 50 then
-    -- Clicked on Refresh
-    enter_name = false
-    refresh()
-  end
-
-  if MOUSE_X > 20 and MOUSE_X < 370 and ktx_mouse_y() > 60 and ktx_mouse_y() < (60 + filecount*20) then
-    -- Clicked on a DAT file
-    enter_name = false
-    clicked_item = math.ceil((ktx_mouse_y()-60)/20)
-    if file_item_clicked == clicked_item then
-      file_item_clicked = 0
+    if MOUSE_X > 0 and MOUSE_X < 90 and ktx_mouse_y() > offset and ktx_mouse_y() < offset+20 then
+      mouse_over_hotspot = true
     else
-      file_item_clicked = math.ceil((ktx_mouse_y()-60)/20)
+      mouse_over_hotspot = false
     end
-  end
-
-  if MOUSE_X > 510 and MOUSE_X < 560 and ktx_mouse_y() > 60 and ktx_mouse_y() < (60 + 4*20) then
-    -- Clicked on a SaveAs button
-    enter_name = false
-    clicked_item = math.ceil((ktx_mouse_y()-60)/20)
-    if save_file_exists[clicked_item] then
-      save_sit(clicked_item,filename_regel)
+  
+  if istate and MOUSE_STATUS == "down" then
+    -- Interface shows
+    
+    if MOUSE_X > 10 and MOUSE_X < 810 and ktx_mouse_y() > offset and ktx_mouse_y() < offset+20 then
+      -- Clicked on Title bar
+      istate = false
     end
-  end
+    
+    if MOUSE_X > 20 and MOUSE_X < 370 and ktx_mouse_y() > 30+offset and ktx_mouse_y() < 50+offset then
+      -- Clicked on Refresh
+      enter_name = false
+      refresh()
+    end
+    
+    if MOUSE_X > 20 and MOUSE_X < 370 and ktx_mouse_y() > 60+offset and ktx_mouse_y() < (60 + filecount*20)+offset then
+      -- Clicked on a DAT file
+      enter_name = false
+      clicked_item = math.ceil((ktx_mouse_y()-60-offset)/20)
+      if file_item_clicked == clicked_item then
+        file_item_clicked = 0
+      else
+        file_item_clicked = math.ceil((ktx_mouse_y()-60-offset)/20)
+      end
+    end
 
-  if MOUSE_X > 400 and MOUSE_X < 500 and ktx_mouse_y() > 60 and ktx_mouse_y() < (60 + 4*20) and file_item_clicked > 0 then
-    -- Clicked on a Load button
-    enter_name = false
-    clicked_item = math.ceil((ktx_mouse_y()-60)/20)
-    load_sit(clicked_item,filelist[file_item_clicked][1])
-  end
+    if MOUSE_X > 510 and MOUSE_X < 560 and ktx_mouse_y() > 60+offset and ktx_mouse_y() < (60 + 4*20)+offset then
+      -- Clicked on a SaveAs button
+      enter_name = false
+      clicked_item = math.ceil((ktx_mouse_y()-60-offset)/20)
+      if save_file_exists[clicked_item] then
+        save_sit(clicked_item,filename_regel)
+      end
+    end
 
-  if MOUSE_X > 400 and MOUSE_X < 500 and ktx_mouse_y() > 160 and ktx_mouse_y() < 180 then
-    -- Clicked on Delete
-    enter_name = false
-    delete_file(filelist[file_item_clicked][1])
-  end
+    if MOUSE_X > 400 and MOUSE_X < 500 and ktx_mouse_y() > 60+offset and ktx_mouse_y() < (60 + 4*20)+offset and file_item_clicked > 0 then
+      -- Clicked on a Load button
+      enter_name = false
+      clicked_item = math.ceil((ktx_mouse_y()-60-offset)/20)
+      load_sit(clicked_item,filelist[file_item_clicked][1])
+    end
 
-  if MOUSE_X > 400 and MOUSE_X < 790 and ktx_mouse_y() > 30 and ktx_mouse_y() < 50 and file_item_clicked == 0 then
-    -- Enter Filename
-    enter_name = true
+    if MOUSE_X > 400 and MOUSE_X < 500 and ktx_mouse_y() > 160+offset and ktx_mouse_y() < 180+offset then
+      -- Clicked on Delete
+      enter_name = false
+      delete_file(filelist[file_item_clicked][1])
+    end
+
+    if MOUSE_X > 400 and MOUSE_X < 790 and ktx_mouse_y() > 30+offset and ktx_mouse_y() < 50+offset and file_item_clicked == 0 then
+      -- Enter Filename
+      enter_name = true
+    else
+      enter_name = false
+    end
   else
-    enter_name = false
+    -- Click Spot to show interface
+    if MOUSE_X > 0 and MOUSE_X < 90 and ktx_mouse_y() > offset and ktx_mouse_y() < offset+20 then
+      istate = true
+    end
   end
-
 end
 
 function FromKeyboard()
